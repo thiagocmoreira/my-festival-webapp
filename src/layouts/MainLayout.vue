fbb2265efa0058ba2c80160b5419e87e75b26b11
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2020-05-11 00:30:44 -0300

    :art: Creating initial index and others additions

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,16 +3,18 @@
     vue-particles(
       color="#fff"
       shape-type="edge"
-      :particles-number="3"
+      :particles-number="6"
       :particle-size="240"
-      :particle-opacity="0.01"
+      :particle-opacity="0.006"
       :line-linked="false"
       :move-speed="3"
       hover-mode="none"
       :click-effect="false"
     ).particles
-    q-page-container.container
-      router-view
+    q-page-container.container.flex
+      //- vertical-menu
+      div.flex-1
+        router-view
 </template>
 
 <script>
@@ -22,7 +24,9 @@ Vue.use(VueParticles)
 
 export default {
   name: 'MainLayout',
-
+  components: {
+    VerticalMenu: () => import('../components/layout/VerticalMenu')
+  },
   data () {
     return {}
   }
@@ -39,7 +43,7 @@ export default {
   background: $grey-10
   position: relative
   height: 100vh
-  // overflow: hidden
+  overflow: hidden
   // pointer-events: none
 
 .particles
@@ -52,9 +56,4 @@ export default {
   top: 0
   width: 100%
   height: 100vh
-  display: flex
-  flex-direction: column
-  align-items: center
-  justify-content: center
-  z-index: 1000
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,17 @@
 <template lang="pug">
-  q-page.column.flex-center.full-width
-    div.text-h3.q-mb-xl.text-white My Festival
-    q-btn(
-      icon="mdi-spotify"
-      color="white"
-      text-color="grey-9"
-      no-caps
-    ) Conecte com o Spotify
+  q-page.index.column
+    div.column.flex-1.flex-center.text-prater.flex-1
+      h1.title.text-lolapeluza.text-bold.text-uppercase.text-white My festival
+      q-btn(
+        color="green-6"
+        no-caps
+        unelevated
+        size="lg"
+      ).button.q-mt-xl
+        div.flex
+          q-icon(name="mdi-spotify").q-mr-sm
+          div Conecte com o Spotify
+    img(src="../assets/multidao.png").crowd-image.absolute.full-width
 </template>
 
 <script>
@@ -14,3 +19,21 @@ export default {
   name: 'PageIndex'
 }
 </script>
+
+<style lang="sass" scoped>
+.index
+  position: relative
+
+.title
+  font-size: 85px
+  line-height: 95px
+  letter-spacing: 0.08em
+  margin: 0
+
+.button
+  padding: 2px 20px
+
+.crowd-image
+  bottom: 0
+  opacity: 0.1
+</style>
