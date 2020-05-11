cd1e2ae2bf622ca5ec65026a9c5cb0ec7c581a12
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2020-05-11 03:17:54 -0300

    :art: Adding bubble button and others adjusts

---
+++ src/components/common/BubbleButton.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="buttons column">
+    <button class="blob-btn">
+      {{ label }}
+      <slot name="content" />
+      <span class="blob-btn__inner">
+        <span class="blob-btn__blobs">
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+        </span>
+      </span>
+    </button>
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+      <defs>
+        <filter id="goo">
+          <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+          <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+          <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+        </filter>
+      </defs>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BubbleButton',
+  props: {
+    label: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.buttons {
+  text-align: center;
+  border-radius:15px;
+  height: 310px;
+  margin-bottom: -300px;
+}
+
+$color: rgb(16, 161, 89);
+$dark: #FFFFFF;
+$borderW: 2px;
+
+.blob-btn {
+  $numOfBlobs: 4;
+  z-index: 1;
+  position: relative;
+  padding: 15px 40px;
+  text-align: center;
+  text-transform: uppercase;
+  color: $color;
+  font-size: 16px;
+  font-weight: bold;
+  background-color: transparent;
+  outline: none;
+  border: none;
+  transition: color 0.5s;
+  cursor: pointer;
+  border-radius:15px;
+
+  &:before {
+    content: "";
+    z-index: 1;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    // border: $borderW solid $color;
+    border-radius:15px;
+  }
+
+  &:after {
+    content: "";
+    z-index: -2;
+    position: absolute;
+    left: $borderW*1.5;
+    top: $borderW*1.5;
+    width: 100%;
+    height: 100%;
+
+    transition: all 0.3s 0.2s;
+    border-radius:15px;
+  }
+
+  &:hover {
+    color: $dark;
+    border-radius:15px;
+
+    &:after {
+      transition: all 0.3s;
+      left: 0;
+      top: 0;
+      border-radius:15px;
+    }
+  }
+
+  &__inner {
+    z-index: -1;
+    overflow: hidden;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    border-radius:15px;
+    background:#ffffff;
+  }
+
+  // additional container created, because in FF blobs are breaking overflow:hidden of element with svg gooey filter
+  &__blobs {
+    position: relative;
+    display: block;
+    height: 100%;
+    filter: url('#goo');
+  }
+
+  &__blob {
+    position: absolute;
+    top: $borderW;
+    width: 100% / $numOfBlobs;
+    height: 100%;
+    background: $color;
+    border-radius: 100%;
+    transform: translate3d(0,150%,0) scale(1.7);
+    transition: transform 0.45s;
+
+    @supports(filter: url('#goo')) {
+      transform: translate3d(0,150%,0) scale(1.4);
+    }
+
+    @for $i from 1 through $numOfBlobs {
+      &:nth-child(#{$i}) {
+        left: ($i - 1) * (120% / $numOfBlobs);
+        transition-delay: ($i - 1) * 0.08s;
+      }
+    }
+
+    .blob-btn:hover & {
+      transform: translateZ(0) scale(1.7);
+      @supports(filter: url('#goo')) {
+        transform: translateZ(0) scale(1.4);
+      }
+    }
+  }
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,25 +1,33 @@
 <template lang="pug">
   q-page.index.column
+    cloud(color="#000").cloud-small--bg.q-ma-md.absolute
     cloud.cloud-small.q-ma-md.absolute
+    cloud(color="#000").cloud-big--bg.q-ma-md.absolute
     cloud.cloud-big.q-ma-md.absolute
     div.column.flex-1.flex-center.text-prater.flex-1
       h1.title.text-lolapeluza.text-bold.text-uppercase.text-white My festival
-      q-btn(
-        color="green-6"
-        no-caps
-        unelevated
-        size="lg"
-      ).button.q-mt-xl
-        div.flex
-          q-icon(name="mdi-spotify").q-mr-sm
-          div Conecte com o Spotify
+      //- q-btn(
+      //-   color="green-7"
+      //-   no-caps
+      //-   unelevated
+      //-   size="lg"
+      //- ).button.q-mt-xl
+      //-   div.flex
+      //-     q-icon(name="mdi-spotify").q-mr-sm
+      //-     div Conecte com o Spotify
+      bubble-button.q-mt-xl
+        template(#content)
+          div.flex.items-center
+            q-icon(name="mdi-spotify" size="28px").q-mr-sm
+            div Conecte com o Spotify
 </template>
 
 <script>
 export default {
   name: 'PageIndex',
   components: {
-    Cloud: () => import('../components/Cloud')
+    Cloud: () => import('../components/common/Cloud'),
+    BubbleButton: () => import('../components/common/BubbleButton')
   }
 }
 </script>
@@ -27,7 +35,7 @@ export default {
 <style lang="sass" scoped>
 .index
   position: relative
-  background: $blue-4
+  background: $blue-5
 
 .title
   font-size: 85px
@@ -43,8 +51,20 @@ export default {
   left: -200px
   top: -50px
 
+  &--bg
+    max-width: 300px
+    left: 10px
+    top: -30px
+    opacity: 0.1
+
 .cloud-big
   bottom: -150px
   max-width: 800px
   right: -290px
+
+  &--bg
+    max-width: 680px
+    bottom: -120px
+    right: -60px
+    opacity: 0.1
 </style>
