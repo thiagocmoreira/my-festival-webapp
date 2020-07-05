5bb611270a0717e99ea09b2671adfdfdb169adef
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2020-07-05 19:35:20 -0300

    :truck: Moving images to new folder and adjusting 404 page

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,24 +1,34 @@
 <template lang="pug">
-  div.fixed-center.text-center
+  div.fixed-center.text-center.text-grey-7
     p
-      img(
-        src="~assets/sad.svg"
-        style="width:30vw;max-width:150px;"
-      )
-    p.text-faded
-      | Sorry, nothing here..
-      strong (404)
-    </p>
+      img(src="~assets/img/sad.svg").sad-img
+    div.label-404.text-bold 404
+    div.text-faded
+      | Desculpe, mas não há nada aqui...
     q-btn(
-      color="secondary"
-      style="width:200px;"
-      to="/"
-      label="Go back"
-    )
+      color="blue-5"
+      unelevated
+      no-caps
+      label="Voltar"
+      @click="$router.push('/')"
+    ).go-back-btn.q-mt-lg
 </template>
 
-<script >
+<script>
 export default {
   name: 'Error404'
 }
 </script>
+
+<style lang="sass" scoped>
+.sad-img
+  width: 30vw
+  max-width: 150px
+
+.label-404
+  font-size: 40px
+  line-height: 40px
+
+.go-back-btn
+  min-width: 200px
+</style>
